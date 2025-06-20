77e508bebc348a31f8ac5adefa54856ad12292db
Thomas <thomasn8255@gmail.com>
2025-06-19 21:25:04 -0700

    added basic introduction with a little bit of styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import HomePage from "./HomePage";
 
 function App() {
   return (
-    <>
-      <div>Personal Portfolio</div>
-    </>
+    <div className="flex items-center justify-center ">
+      <HomePage name="Thomas Nguyen" />
+    </div>
   );
 }
 
---
+++ src/HomePage.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import profilePic from "./assets/profilepic.jpg";
+
+interface HomePageProps {
+  name: string;
+}
+
+const HomePage: React.FC<HomePageProps> = ({ name }) => {
+  return (
+    <section className="flex flex-row items-center gap-x-18 py-80 p-40">
+      <div className="flex flex-col gap-y-.5">
+        <h1 className="text-6xl font-bold text-gray-800">{name}</h1>
+        <h2 className="text-2xl text-gray-600 mt-1 ">
+          Computer Science Student at UCI
+        </h2>
+      </div>
+      <img
+        src={profilePic}
+        alt="profilepic"
+        className="w-54 h-54 rounded-full object-cover shadow-lg"
+      />
+    </section>
+  );
+};
+
+export default HomePage;
