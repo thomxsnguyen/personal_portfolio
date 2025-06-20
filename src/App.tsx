cf631ef13806fe326f70b35b8dc465c4f8a8c716
Thomas <thomasn8255@gmail.com>
2025-06-19 21:55:14 -0700

    added nav bar and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import HomePage from "./HomePage";
+import NavBar from "./NavBar";
 
 function App() {
   return (
-    <div className="flex items-center justify-center ">
+    <div className="flex items-center justify-center flex-col">
+      <NavBar />
       <HomePage name="Thomas Nguyen" />
     </div>
   );
